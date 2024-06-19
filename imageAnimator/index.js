17b93730154dd90819b8e0255a0daa7792efa40f
Aaron Sala <salaaron2@gmail.com>
2024-06-20 02:09:04 +0300

    did all the animations of the images

---
+++ imageAnimator/index.js
@@ -0,0 +1,13 @@
+let nextDom = document.getElementById("next");
+let prevDom = document.getElementById("prev");
+let carousolDom = document.querySelector('.carousal')
+let listItemDom = document.querySelector('.carousol .list');
+let thumbnailDom = document.querySelector('.carousol .thumbnail');
+
+nextDom.onclick = function () {
+    showSlider('next')
+}
+
+showSlider=()=> {
+    
+}
